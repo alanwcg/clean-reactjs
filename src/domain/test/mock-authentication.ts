c1d2f51282acdf840d49b43691f5d1d01553e77f
alanwcg <alancintra7@gmail.com>
2022-03-30 07:43:33 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct body

---
+++ src/domain/test/mock-authentication.ts
@@ -0,0 +1,6 @@
+import { AuthenticationParams } from 'domain/usecases/authentication'
+
+export const mockAuthentication = (): AuthenticationParams => ({
+  email: 'kagru@baskozvi.nc',
+  password: '3KPWYyd3'
+})
